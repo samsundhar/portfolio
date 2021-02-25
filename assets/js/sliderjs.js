3aa992c55f1e399d7453605db36a1e3716a7ea68
unknown <pranavmappoli@gmail.com>
2021-02-26 00:42:51 +0530

    initial commit

---
+++ assets/js/sliderjs.js
@@ -0,0 +1,6 @@
+
+	//JSON PORTFOLIO
+	var portfolioJSON = [{"0":"Mother's Ring Builder","4":"2013-03-25 20:00:50","1":"2013\/03\/mothers.jpg","3":"191"},{"0":"BCI Rentals - Custom CMS","4":"2012-10-28 19:24:24","1":"2013\/03\/bci.jpg","3":"201"},{"0":"Schoolwires Creative Services","4":"2012-03-07 03:07:55","1":"2012\/03\/creative.jpg","2":"#FFFFFF","3":"82"},{"0":"Self-Service","4":"2012-03-06 16:43:34","1":"2012\/03\/selfservice11.jpg","2":"#669900","3":"4"},{"0":"Portfolio: \"Infinite Scolling\" Wall","4":"2012-03-05 16:44:51","1":"2012\/03\/portfolio1.jpg","2":"#304B89","3":"176"}];
+	var featuresTimer;
+	var resizeTimer;
+	var hoverTimer;
